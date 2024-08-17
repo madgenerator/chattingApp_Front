afe10ea502d2d26bca5041c06d4c94e98915a0a2
SONG JUNBONG <53103084+madgenerator@users.noreply.github.com>
2024-08-18 01:52:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const userID = document.getElementById("userID");
 //addEventListner to "userMessage"
 userInput.addEventListener("keydown", messageEnter);
 
-const socket = io("https://chattingv1-7e9e3dc81fb7.herokuapp.com");
+const socket = io("https://port-0-chattingapp-lzycyeal2a8e816d.sel4.cloudtype.app");
 
 function messageEnter(event){
   if(event.key == "Enter"){
